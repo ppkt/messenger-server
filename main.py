0a74bd61793b96146c5a6837963e848a02dc9a13
ppkt <github@ppkt.eu>
2014-04-27 08:22:28 +0200

    New method _clientSend

--- main.py
+++ main.py
@@ -7,14 +7,22 @@ from twisted.internet.protocol import Protocol, Factory
 
 
 class Echo(Protocol):
+    # Wrapper for encoding
+    @classmethod
+    def _clientSend(cls, handler, content):
+        if not content.endswith("\r\n"):
+            content +=  "\r\n"
+        handler.write(content)
+
+
     def __init__(self, factory):
         # Assign Factory and create TimerService (for heartbeats)
         self.factory = factory
-        self.timer = internet.TimerService(10, self.hearbeat)
+        self.timer = internet.TimerService(10, self.heartbeat)
 
     # Send heartbeat
-    def hearbeat(self):
-        self.transport.write("Ping\r\n")
+    def heartbeat(self):
+        self._clientSend(self.transport, "Ping")
 
     def connectionMade(self):
         # Run heartbeat service
@@ -22,27 +30,27 @@ class Echo(Protocol):
 
         # Announce new presence
         for client in self.factory.clients:
-            client.write("New user!\r\n")
+            self._clientSend(client, "New user")
 
         # Add user to roster
         self.factory.clients.append(self.transport)
 
         # Send greeting to user
-        self.transport.write("Hi, currently there are %d clients\r\n" % (len(self.factory.clients)))
+        self._clientSend(self.transport, "Hi, currently there are %d clients" % (len(self.factory.clients)))
 
     def connectionLost(self, reason):
         # Remove user from roster and send announcement to other users
         self.factory.clients.remove(self.transport)
         for client in self.factory.clients:
-            client.write("Client left server\r\n")
+            self._clientSend(client, "Client left server")
 
     # Send any received data to all clients
     def dataReceived(self, data):
         for client in self.factory.clients:
             if client != self.transport:
-                client.write(data)
+                self._clientSend(client, data)
             else:
-                self.transport.write("*" + data)
+                self._clientSend(self.transport, "*" + data)
 
 class EchoFactory(Factory):
     def __init__(self):
